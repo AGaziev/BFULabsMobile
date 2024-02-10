608413a6481a9cbbb57be0d23a43a07e6a75131b
Алан Газиев <alan.gaziev27@gmail.com>
2024-02-10 13:55:53 +0200

    two last problems for lab1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import kotlin.arrayOf as arrayOf
 
 fun main() {
@@ -5,6 +6,9 @@ fun main() {
     println(logMinMax())
     val numToCheckOnTwiceEvenable = readln().toInt()
     println(TwiceEvenNumDeterminator().IsTwiceEven(numToCheckOnTwiceEvenable))
+
+    println(maxLengthUniqStr(readln()))
+    println(maxInRow(readln().toInt(), readln().toInt()))
 }
 
 fun sumOfSeries(): Int {
@@ -75,4 +79,41 @@ class TwiceEvenNumDeterminator()
     private fun isEven(num: Int): Boolean {
         return num%2==0
     }
+}
+
+fun maxLengthUniqStr(string: String): String {
+    var strToSearch = string
+    var start = 0
+    var substrings: List<String> = ArrayList()
+    var biggestUniq = StringBuilder()
+    var i=0
+    while (i<string.length) {
+        if (string[i] !in biggestUniq)
+        {
+            biggestUniq.append(string[i])
+            i++
+        }
+        else
+        {
+            substrings.addLast(biggestUniq.toString())
+            var offset = biggestUniq.indexOfFirst { char -> char==string[i] }
+            start += offset + 1
+            biggestUniq.clear()
+            i = start
+        }
+    }
+    substrings.addLast(biggestUniq.toString())
+    return substrings.maxBy { str -> str.length }
+}
+
+fun maxInRow(n: Int, m: Int) : List<Int>{
+    val maxes : List<Int> = ArrayList()
+    val array = getRandomArrays(n, m)
+    println(array.contentDeepToString())
+    val a = array.fold(maxes, { a, b -> a.plus(b.max()) })
+    return a
+}
+
+fun getRandomArrays(n: Int, m: Int): Array<Array<Int>> {
+    return Array(n) { Array(m){ Random.nextInt(0,100) } }
 }
